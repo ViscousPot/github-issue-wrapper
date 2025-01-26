0eae65255a53d3133014ab78edc63549efbdc89d
ViscousPot <viscouspotential@gmail.com>
2025-01-26 04:29:09 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,35 @@ import { Edit } from './pages/Edit'
 import { useSearchParams } from 'react-router-dom';
 import { Data } from './types';
 import * as JSURL from "jsurl2"
+import { useEffect } from 'react';
 
 export function App() {
   const [searchParams] = useSearchParams();
 
   const close = searchParams.get("close");
 
-  if (close != null) {
+  if (close) {
     window.open("about:blank", "_self");
     window.close();
   }
 
   const code = searchParams.get("code");
 
-  if (code != null) {
-    const asyncSaveAuth = async () => {
-      const response = await fetch(`/access_token/${code}`)
+  useEffect(() => {
+    if (code) {
+      console.log("test1")
+      const asyncSaveAuth = async () => {
+        const response = await fetch(`/access_token/${code}`)
 
-      localStorage.setItem('gitHub_access_token', response.toString());
-      window.location.href = "https://github.com/apps/issue-wrapper/installations/new", "_self"
+        localStorage.setItem('gitHub_access_token', await response.text());
+        window.location.href = "https://github.com/apps/issue-wrapper/installations/new", "_self"
+      }
+
+      asyncSaveAuth()
     }
+  }, [])
 
-    asyncSaveAuth()
-
+  if (code) {
     return null
   }
 
