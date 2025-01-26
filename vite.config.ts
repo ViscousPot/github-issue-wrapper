063fd83d538d45224433350d2636ebafdf0be3c9
ViscousPot <viscouspotential@gmail.com>
2025-01-26 01:53:23 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/github-issue-wrapper/',
+  base: '/github-issue-wrapper',
   plugins: [react(), tailwindcss(),],
 })
