a810ec8ccbd848c5d580796aa03bc02be152d4e2
ViscousPot <viscouspotential@gmail.com>
2025-02-06 21:55:14 +0000

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 
 
 export const CLIENT_ID = "Iv23lilRcPa61LZU1Py0"
-export const REDIRECT_URI = "https://issue-wrapper.netlify.app/"
+export const REDIRECT_URI = "https://issue-wrapper.viscouspotenti.al/"
